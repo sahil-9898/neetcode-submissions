b01e50f8e92ab9a16a2b3ed2a7685ca9ab9aeba8
Sahil Tiwari <60009481+sahil-9898@users.noreply.github.com>
2026-05-03 17:37:41 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int, unordered_set<char>> row;
+        unordered_map<int, unordered_set<char>> column;
+        map<pair<int, int>, unordered_set<char>> box;
+        
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                pair<int, int> boxIndex = {i/3, j/3};
+                if (row[i].find(c) != row[i].end() || column[j].find(c) != column[j].end() || box[boxIndex].find(c) != box[boxIndex].end()) {
+                    return false;
+                }
+                row[i].insert(c);
+                column[j].insert(c);
+                box[boxIndex].insert(c);
+            }
+        }
+        return true;
+    }
+};
