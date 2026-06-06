20ea8a44da40eb4607a22abb59266f4d65b3cf91
Sahil Tiwari <60009481+sahil-9898@users.noreply.github.com>
2026-06-06 12:41:43 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int left = 0;
+        int right = s1.length() - 1;
+        sort(s1.begin(), s1.end());
+        while (right < s2.length()) {
+            string subStr = s2.substr(left, s1.length());
+            right++;
+            left++;
+            sort(subStr.begin(), subStr.end());
+            if (s1 == subStr) return true;
+        }
+        return false;
+    }
+};
