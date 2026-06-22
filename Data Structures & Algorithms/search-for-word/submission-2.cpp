5ceaad73e7c476dde2bab22285f620c7565a145c
Sahil Tiwari <60009481+sahil-9898@users.noreply.github.com>
2026-06-22 21:54:26 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    vector<vector<bool>> visited;
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        visited.assign(m, vector<bool>(n, false));
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (bfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+private:
+    bool bfs(vector<vector<char>>& board, string word, int x, int y, int i) {
+        int m = board.size();
+        int n = board[0].size();
+
+        if (x >= m || y >= n || x < 0 || y < 0 || visited[x][y] || board[x][y] != word[i]) {
+            return false;
+        }
+
+        if (i == word.length() - 1) return true;
+
+        visited[x][y] = true;
+        bool a = bfs(board, word, x+1, y, i + 1);
+        bool b = bfs(board, word, x-1, y, i + 1);
+        bool cc = bfs(board, word, x, y+1, i + 1);
+        bool d = bfs(board, word, x, y-1, i + 1);
+        visited[x][y] = false;
+        return a || b || cc || d;
+    }
+};
